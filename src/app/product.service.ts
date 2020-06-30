775229ca76c5ce515dc203ffcc991d87b310d641
humbertomartinezg <humbertomartinezg@gmail.com>
2020-06-29 22:01:53 -0500

    .

---
+++ src/app/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Observable } from 'rxjs/Observable';
+
+
+
+@Injectable()
+export class ProductService {
+
+  
+  private _albumUrl : string = '../assets/album.json';
+  private _response : any;
+  constructor(private _http: Http) { }
+
+  getAlbum(id:number):Observable<any>{        
+    return this._http.get(this._albumUrl)
+    .map((res : any) => res.json());    
+  }
+
+}
