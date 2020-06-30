7d908eede2477f6c6e1ad8dd798bb10f6c719831
humbertomartinezg <humbertomartinezg@gmail.com>
2020-06-29 22:10:43 -0500

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -15,7 +15,7 @@ export class ProductService {
   private _response : any;
   constructor(private _http: Http) { }
 
-  getAlbum(id: number):Observable<any>{        
+  getAlbum(id: number){        
     return this._http.get(this._albumUrl)
     .map((response : any) => response.json());    
   }
